86127979976ba469be4bf9ef4bb23cc7e63550de
anis-agwan <59297399+anis-agwan@users.noreply.github.com>
2019-12-29 01:53:11 +0530

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','berkeley.edu','cam.ac.uk','mac.com','ucdavis.edu','gmail.com','uct.ac.za','ufp.pt','yale.edu'],
+['2005',57,12,12,7,12,11,10,14,10,5],
+['2006',387,137,123,125,111,101,91,82,70,75]
+];
